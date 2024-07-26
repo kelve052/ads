8f06a8986979af0bb1864dbf1fc959dd329d7fc8
Kelve Oliveira <kelveoliveira052@gmail.com>
2024-07-25 23:52:08 -0300

    update

--- marco/aula 2 java script/assets/script2.js
+++ marco/aula 2 java script/assets/script2.js
@@ -4,6 +4,10 @@ const inputAno = document.getElementById('ano')
 
 const btn = document.getElementById('btn')
 
+const tabela = document.getElementById('body-table')
+
+const caixaMensagen = document.getElementById('caixa-mensagen')
+
 console.log(inputTitulo)
 console.log(inputAutor)
 console.log(inputAno)
@@ -12,23 +16,38 @@ inputTitulo.value = 'Blibia'
 inputAutor.value = 'Deus'
 inputAno.value = '658'
 
-console.log(inputTitulo.value)
-console.log(inputAutor.value)
-console.log(inputAno.value)
 
 
-const livros = []
 const addLivro = (titulo, autor, ano)=>{
-    const livro = {
-        titulo: titulo,
-        autor: autor,
-        ano: ano
-    }
-    livros.push(livro)
+    const tdTitulo = document.createElement('td')
+    const tdAutor = document.createElement('td')
+    const tdAno = document.createElement('td')
+    tdTitulo.innerText = titulo
+    tdAutor.innerText = autor
+    tdAno.innerText = ano
+
+    tdTitulo.className = 'dados-livro'
+    tdAutor.className = 'dados-livro'
+    tdAno.className = 'dados-livro'
+
+    const linhaDeCadaLivro = document.createElement('tr')
+    linhaDeCadaLivro.appendChild(tdTitulo)
+    linhaDeCadaLivro.appendChild(tdAutor)
+    linhaDeCadaLivro.appendChild(tdAno)
+
+    linhaDeCadaLivro.className = 'linha-de-cada-livro'
+
+    tabela.appendChild(linhaDeCadaLivro)
+
+    caixaMensagen.style.top = '6px'
+
+    setTimeout(() => {
+        caixaMensagen.style.top = '-500px'
+    }, 3000);
 }
 
-document.addEventListener('mousemove', (e)=>{
-    addLivro(inputTitulo.value, inputAutor.value, inputAno.value,)
+btn.addEventListener('click', (e)=>{
+    addLivro(inputTitulo.value, inputAutor.value, inputAno.value)
     e.preventDefault()
     console.table(livros)
 })
